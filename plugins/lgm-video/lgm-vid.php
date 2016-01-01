af9e3f4e3b996ae0804d087768ea62252c9b3573
Manuel Schmalstieg <git.code@ms-studio.net>
2016-01-01 17:57:16 +0100

    removing legacy files.

--- plugins/lgm-video/lgm-vid.php
+++ plugins/lgm-video/lgm-vid.php
@@ -8,10 +8,6 @@ Author: LGM Infrastructure Team
 Author URI: https://github.com/libregraphicsmeeting/infrastructure/
 */
 
- 
- // Custom Post Types
- // include_once (plugin_dir_path(__FILE__).'lgm-custom-post-types.php');
- 
  // Speakers
  include_once (plugin_dir_path(__FILE__).'lgm-speakers.php');
  
